c3515a091111be754ac4a13efb67bb9940d95423
tonigarioliver <tonietmoscari@gmail.com>
2022-10-01 14:33:26 +0200

    Exercise 2 init

--- src/Exercise2/Main.java
+++ src/Exercise2/Main.java
@@ -3,6 +3,8 @@ package Exercise2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Exercise 2");
+
+        System.out.println("Exercise 2");
     }
 
 }
