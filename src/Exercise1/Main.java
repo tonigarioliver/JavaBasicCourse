cf324ffeffa34f176ddaec8fabcedc223cdda40c
tonigarioliver <tonietmoscari@gmail.com>
2022-10-01 14:21:49 +0200

    Exercise finished

--- src/Exercise1/Main.java
+++ src/Exercise1/Main.java
@@ -1,2 +1,18 @@
-package Exercise1;public class Main {
+package Exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        Print p = new Print();
+        int i = 0;
+        p.printvalue(i);
+        String j =Integer.toString(i);
+        p.printvalue(j);
+        float k = Float.parseFloat(j);
+        p.printvalue(k);
+        boolean l = true;
+        p.printvalue(l);
+        long m = Long.parseLong(j);
+        p.printvalue(m);
+
+    }
 }
--- src/Exercise1/Print.java
+++ src/Exercise1/Print.java
@@ -1,2 +1,23 @@
-package Exercise1;public class Print {
+package Exercise1;
+
+public class Print {
+    public static void main(String[] args) {
+    }
+
+    protected void printvalue(int i) {
+        System.out.println(i);
+    }
+    protected void printvalue(String i) {
+        System.out.println(i);
+    }
+    protected void printvalue(long i) {
+        System.out.println(i);
+    }
+    protected void printvalue(boolean i) {
+        System.out.println(i);
+    }
+    protected void printvalue(float i) {
+        System.out.println(i);
+    }
+
 }
