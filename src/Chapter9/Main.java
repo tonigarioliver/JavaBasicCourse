38b5abfa4ad94d1906d6be5e8ef7b06d27235864
tonigarioliver <tonietmoscari@gmail.com>
2022-10-23 20:16:52 +0200

    Course finished

---
+++ src/Chapter9/Main.java
@@ -0,0 +1,12 @@
+package Chapter9;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce tu nombre");
+        String nombre = input.nextLine();
+    }
+}
